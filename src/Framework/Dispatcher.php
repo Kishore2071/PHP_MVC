8b8eb728c1a8d6c94e9c33591747ba66867751f5
Kishore2071 <kishorekumarsaran2001@gmail.com>
2024-07-28 17:54:01 +0530

    Dependency injection

--- src/Framework/Dispatcher.php
+++ src/Framework/Dispatcher.php
@@ -3,6 +3,7 @@
 namespace Framework;
 
 use ReflectionMethod;
+use ReflectionClass;
 
 class Dispatcher
 {
@@ -23,7 +24,7 @@ class Dispatcher
         $action = $this->getActionName($params);
         $controller = $this->getControllerName($params);
 
-        $controller_object = new $controller;
+        $controller_object = $this->getObject($controller);
 
         $args = $this->getActionArguments($controller, $action, $params);
 
@@ -71,5 +72,30 @@ class Dispatcher
         $action = lcfirst(str_replace("-", "", ucwords(strtolower($action), "-")));
 
         return $action;
+    }
+
+    private function getObject(string $class_name): object
+    {
+        $reflector = new ReflectionClass($class_name);
+
+        $constructor = $reflector->getConstructor();
+
+        $dependencies = [];
+
+        if ($constructor === null) {
+
+            return new $class_name;
+
+        }
+
+        foreach ($constructor->getParameters() as $parameter) {
+
+            $type = (string) $parameter->getType();
+
+            $dependencies[] = $this->getObject($type);
+
+        }
+
+        return new $class_name(...$dependencies);
     }
 }
